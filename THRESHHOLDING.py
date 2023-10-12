0a2fcd6c788ecce350b8b2cae85362f925856011
Leon Mario <l.gahn@reisinger.de>
2023-10-12 12:45:25 +0200

    Blended 2er Bilder

--- THRESHHOLDING.py
+++ THRESHHOLDING.py
@@ -8,22 +8,19 @@ img1 = cv2.imread(
 
 # img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
 
-ret, thresh1 = cv2.threshold(img1, 127, 255, cv2.THRESH_TRUNC) #param1 = imput image oaram 2 = threshold very value above 127 get turned to max , below to zero, param 3 max val
+# param1 = imput image oaram 2 = threshold very value above 127 get turned to max , below to zero, param 3 max val
+ret, th1 = cv2.threshold(img1, 127, 255, cv2.THRESH_BINARY)
+th2 = cv2.adaptiveThreshold(
+    img1, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 8)
 
-print(ret)
-plt.imshow(thresh1,cmap="gray")
-plt.show()
-
-
-
+blended = cv2.addWeighted(src1=th1, alpha=0.6, src2=th2, beta=0.4, gamma=0)
 
 
+def show_image(img):
+    fig = plt.figure(figsize=(15, 15))
+    ax = fig.add_subplot(111)
+    ax.imshow(img, cmap="gray")
+    plt.show()
 
 
-
-
-
-
-
-
-
+show_image(blended)
